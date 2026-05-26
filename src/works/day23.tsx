1409beee89c151717f613abc06806e4ce11282f8
mgmg92-web <mgmg92@woowahan.com>
2026-05-27 07:31:10 +0900

    day23: FROM 링크 추가 + md 규칙 보완

---
+++ src/works/day23.tsx
@@ -0,0 +1,9 @@
+export default function Day023() {
+  return (
+    <iframe
+      src={`${import.meta.env.BASE_URL}works/day23/index.html`}
+      style={{ width: '100%', height: '100%', border: 'none' }}
+      title="Day 23: Knitting Typography"
+    />
+  )
+}
